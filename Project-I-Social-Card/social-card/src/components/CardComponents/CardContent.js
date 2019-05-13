23128561227439d3eda0470f9e3aea12439f327e
SachinBenny <sachben91@gmail.com>
2019-05-12 19:57:07 -0500

    First React commit

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+const CardContent = () => {
+    return (
+        <div>
+            <h2>Get started with Reach</h2>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p> 
+            <span>reactjs.org</span>
+        </div>
+    )
+}
+export default CardContent
